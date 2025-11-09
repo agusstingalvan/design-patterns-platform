fe7a1da961e6917dbeed16ed4d15f9beaf3ebeb0
Agustin <agusstingalvan2@gmail.com>
2025-11-08 23:22:08 -0300

    fix(ui): update text in SiteFooter component

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -8,8 +8,8 @@ export function SiteFooter() {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <GitFork className="h-6 w-6" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built for game developers to implement design patterns of
-            videogames.
+            Diseñado para desarrolladores de videojuegos para implementar
+            patrones de diseño. ❤️
           </p>
         </div>
         {/* <div className="flex flex-col items-center gap-4 md:flex-row md:gap-6">
